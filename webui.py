35e5fc0f0e59b36b9da190573f6b56bcdac39036
xieyongliang <yongliangxie@hotmail.com>
2023-03-26 22:13:11 +0800

    fix the model.tar.gz with the correct directory structure

--- webui.py
+++ webui.py
@@ -746,19 +746,21 @@ if cmd_opts.train:
                     )
                 #automatic tar latest checkpoint and upload to s3 by zheng on 2023.03.22
                 os.makedirs(os.path.dirname("/opt/ml/model/"), exist_ok=True)
+                os.makedirs(os.path.dirname("/opt/ml/model/Stable-diffusion/"), exist_ok=True)
+                os.makedirs(os.path.dirname("/opt/ml/model/Lora/"), exist_ok=True)
                 train_steps=int(db_config.revision)
                 model_file_basename = f'{db_model_name}_{train_steps}_lora' if db_config.use_lora else f'{db_model_name}_{train_steps}'
                 f1=os.path.join(sd_models_dir, db_model_name, f'{model_file_basename}.yaml')
                 if os.path.exists(f1):
-                    shutil.copy(f1,"/opt/ml/model/")
+                    shutil.copy(f1,"/opt/ml/model/Stable-diffusion/")
                 if db_save_safetensors:
                     f2=os.path.join(sd_models_dir, db_model_name, f'{model_file_basename}.safetensors')
                     if os.path.exists(f2):
-                        shutil.copy(f2,"/opt/ml/model/")
+                        shutil.copy(f2,"/opt/ml/model/Stable-diffusion/")
                 else:
                     f2=os.path.join(sd_models_dir, db_model_name, f'{model_file_basename}.ckpt')
                     if os.path.exists(f2):
-                        shutil.copy(f2,"/opt/ml/model/")
+                        shutil.copy(f2,"/opt/ml/model/Stable-diffusion/")
             except Exception as e:
                 traceback.print_exc()
                 print(e)
