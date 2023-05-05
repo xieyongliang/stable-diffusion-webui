fe4cd9af49d2f41db8090df5d1e365b96b8f28dd
xieyongliang <yongliangxie@hotmail.com>
2023-05-05 22:31:43 +0800

    fix login issues

--- javascript/ui.js
+++ javascript/ui.js
@@ -224,7 +224,7 @@ function login(username) {
     var user=gradioApp().querySelector('#tabs').querySelectorAll('button')[5];
     var setting=gradioApp().querySelector('#tabs').querySelectorAll('button')[6];
 
-    if(username=='admin'){
+    if(username=='<h1>admin</h1>'){
         user.style.display='block'
         setting.style.display='none'
         user.click()
